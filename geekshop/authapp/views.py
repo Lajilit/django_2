321a3d6e47824c8f143ae56cd6d6c00e41b88615
Lajilit <lajilit@gmail.com>
2021-07-05 22:58:06 +0300

    Домашняя работа к уроку 2 Обновила SMTP-сервер, раньше был гугл, теперь мой собственный

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -111,7 +111,8 @@ def verify(request, email, activation_key):
                 not user.is_activation_key_expired():
             user.is_active = True
             user.save()
-            auth.login(request, user)
+            auth.login(request, user,
+                       backend='django.contrib.auth.backends.ModelBackend')
             return render(request, 'authapp/verification.html')
         else:
             print(f'error activation user: {user}')
--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -157,7 +157,7 @@ EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = env('EMAIL_PORT')
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.AllowAllUsersModelBackend',
